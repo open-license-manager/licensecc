249c896cb1d88867207c29ce2f302490f128a483
Gabriele Contini <contini.mailing@gmail.com>
2019-09-06 23:35:37 +0800

    windows 11th

--- windows_travis.bat
+++ windows_travis.bat
@@ -4,6 +4,6 @@ if not exist "C:/local/boost_1_68_0/build" (
 		wget https://dl.bintray.com/boostorg/release/1.68.0/source/boost_1_68_0.7z
 		7z x boost_1_68_0.7z -o/C/local
 		cd '/C/local/boost_1_68_0' 
-		./bootstrap.bat 
-		./b2.exe --with-date_time --with-test --with-filesystem --with-program_options --with-regex --with-serialization --with-system runtime-link=static 
+		bootstrap.bat 
+		b2.exe --with-date_time --with-test --with-filesystem --with-program_options --with-regex --with-serialization --with-system runtime-link=static 
 )

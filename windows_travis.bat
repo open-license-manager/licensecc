9df3e54921c28cdc0721f09d2704f61191d3af02
Gabriele Contini <contini.mailing@gmail.com>
2019-09-07 08:52:49 +0800

    windows 14

--- windows_travis.bat
+++ windows_travis.bat
@@ -1,10 +1,14 @@
 @echo off
-if not exist "C:/local/boost_1_68_0/build/stage" ( 
+if not exist "C:/local/boost/include" (
         echo "Boost not cached, compiling it"
 		wget https://dl.bintray.com/boostorg/release/1.68.0/source/boost_1_68_0.7z
-		7z x boost_1_68_0.7z -oC:/local
+		cd "C:/local/boost_1_68_0"
+		dir 
+		7z x boost_1_68_0.7z -bb1 -oC:/local
 		cd "C:/local/boost_1_68_0"
 		dir 
 		C:/local/boost_1_68_0/bootstrap.bat 
-		C:/local/boost_1_68_0/b2.exe --with-date_time --with-test --with-filesystem --with-program_options --with-regex --with-serialization --with-system runtime-link=static 
+		C:/local/boost_1_68_0/b2.exe --with-date_time --with-test --with-filesystem --with-program_options --with-regex --with-serialization --with-system runtime-link=static --prefix=C:/local/boost install 
+		cd C:/local/boost
+		dir
 )

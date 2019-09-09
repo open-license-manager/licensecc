79611f5922649b48cc308198d6ea6b1ef3ba1687
Gabriele Contini <contini.mailing@gmail.com>
2019-09-09 15:56:04 +0800

    boost binary dist download

--- windows_travis_build_boost.bat
+++ windows_travis_build_boost.bat
@@ -1,17 +1,19 @@
 @echo off
 REM build and cache boost on Windows N.B. if no output for 10 minutes it's killed by travis. 
 REM if set every command to verbose it's killed too for "too much output"...
-if not exist "C:/local/boost/include" (
+if not exist "C:/local/boost/libs" (
         echo "Boost not cached, compiling it"
-		wget -q https://dl.bintray.com/boostorg/release/1.68.0/source/boost_1_68_0.7z
+		rem wget -q https://dl.bintray.com/boostorg/release/1.71.0/source/boost_1_71_0.7z
+		wget -q -O boost.exe https://dl.bintray.com/boostorg/release/1.71.0/binaries/boost_1_71_0-msvc-14.1-64.exe
 		echo "Boost downloaded"
-		7z x boost_1_68_0.7z -bb1 -oC:/local | C:\Windows\System32\findstr.exe /r /c:”cpp”
-		cd "C:/local/boost_1_68_0"
-		C:/local/boost_1_68_0/bootstrap.bat 
-		C:/local/boost_1_68_0/b2.exe --with-date_time --with-test --with-filesystem --with-program_options --with-regex --with-serialization --with-system runtime-link=static --prefix=C:/local/boost stage
-		echo "Boost compiled"
-		C:/local/boost_1_68_0/b2.exe --with-date_time --with-test --with-filesystem --with-program_options --with-regex --with-serialization --with-system runtime-link=static --prefix=C:/local/boost install > a.txt 2>&1 
+		rem 7z x boost_1_71_0.7z -bb1 -oC:/local | C:\Windows\System32\findstr.exe /r /c:”cpp”
+		rem cd "C:/local/boost_1_71_0"
+		rem C:/local/boost_1_71_0/bootstrap.bat 
+		rem C:/local/boost_1_71_0/b2.exe -j16 toolset=msvc-14.1 address-model=64 architecture=x86 link=static threading=multi runtime-link=static --with-date_time --with-test --with-filesystem --with-program_options --with-regex --with-serialization --with-system --prefix=C:/local/boost stage
+		rem echo "Boost compiled"
+		rem C:/local/boost_1_71_0/b2.exe -j16 toolset=msvc-14.1 address-model=64 architecture=x86 link=static threading=multi runtime-link=static --with-date_time --with-test --with-filesystem --with-program_options --with-regex --with-serialization --with-system --prefix=C:/local/boost install > a.txt 2>&1
+		boost.exe /SILENT /DIR=C:\local\boost 
 		echo "Boost installed"
 		cd C:/local/boost
 		dir
-)
+) else echo "Boost already installed"

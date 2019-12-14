f51df4dc85c81496c8ca5db35b51979e0d917635
gcontini <1121667+gcontini@users.noreply.github.com>
2019-12-14 19:17:07 +0800

    codacy warnings

--- src/library/locate/LocatorFactory.cpp
+++ src/library/locate/LocatorFactory.cpp
@@ -21,14 +21,18 @@ bool LocatorFactory::find_license_with_env_varb = FIND_LICENSE_WITH_ENV_VAR;
 FUNCTION_RETURN LocatorFactory::get_active_strategies(std::vector<std::unique_ptr<LocatorStrategy>> &strategies,
 													  const LicenseLocation *locationHint) {
 	if (find_license_near_moduleb) {
-		strategies.push_back(std::unique_ptr<LocatorStrategy>((LocatorStrategy *)new ApplicationFolder()));
+		strategies.push_back(
+			std::unique_ptr<LocatorStrategy>(dynamic_cast<LocatorStrategy *>(new ApplicationFolder())));
 	}
 	if (find_license_with_env_varb) {
-		strategies.push_back(std::unique_ptr<LocatorStrategy>((LocatorStrategy *)new EnvironmentVarLocation()));
-		strategies.push_back(std::unique_ptr<LocatorStrategy>((LocatorStrategy *)new EnvironmentVarData()));
+		strategies.push_back(
+			std::unique_ptr<LocatorStrategy>(dynamic_cast<LocatorStrategy *>(new EnvironmentVarLocation())));
+		strategies.push_back(
+			std::unique_ptr<LocatorStrategy>(dynamic_cast<LocatorStrategy *>(new EnvironmentVarData())));
 	}
 	if (locationHint != nullptr) {
-		strategies.push_back(std::unique_ptr<LocatorStrategy>((LocatorStrategy *)new ExternalDefinition(locationHint)));
+		strategies.push_back(
+			std::unique_ptr<LocatorStrategy>(dynamic_cast<LocatorStrategy *>(new ExternalDefinition(locationHint))));
 	}
 	return strategies.size() > 0 ? FUNC_RET_OK : FUNC_RET_NOT_AVAIL;
 }

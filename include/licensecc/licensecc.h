5081f5a80e62322155736ddf5c12140627aff2aa
gcontini <1121667+gcontini@users.noreply.github.com>
2020-01-11 14:44:17 +0800

    issue #70
    #define constants for api arrays

--- include/licensecc/licensecc.h
+++ include/licensecc/licensecc.h
@@ -2,8 +2,8 @@
 #define LICENSEPP_H_
 
 /*
- * This include file is the public api di License++
-*/
+ * This include file is the public api di Licensecc
+ */
 #ifdef __cplusplus
 extern "C" {
 #endif
@@ -12,15 +12,16 @@ extern "C" {
 
 /*
  * Method used to convert the LicenseInfo into a human readable
- * representation.
+ * representation. //not yet implemented
  */
-void print_error(char out_buffer[256], LicenseInfo* licenseInfo);
+void print_error(char out_buffer[ERROR_BUFFER_SIZE], LicenseInfo* licenseInfo);
 
 /**
- * This method calculate the pc identifier. The string has to be shown
- * to the user in order to calculate the license.
+ * This method calculates the pc identifier. The string need to be shown to the user and given back to the software
+ * editor when issuing a license.
+ *  pc_id_method = STRATEGY_DEFAULT usually works.
  */
-bool identify_pc(IDENTIFICATION_STRATEGY pc_id_method, char* identifier_out, size_t bufSize);
+bool identify_pc(IDENTIFICATION_STRATEGY pc_id_method, char* identifier_out, size_t* bufSize);
 
 /**
  * This method is used to request the use of one license for a product.
@@ -43,13 +44,11 @@ EVENT_TYPE acquire_license(const CallerInformations* callerInformation, const Li
  * Should be called from time to time to confirm we're still using the
  * license.
  */
-EVENT_TYPE confirm_license(char * featureName,
-		LicenseLocation* licenseLocation);
+EVENT_TYPE confirm_license(char* featureName, LicenseLocation* licenseLocation);
 /**
  * Do nothing for now, useful for network licenses.
  */
-EVENT_TYPE release_license(char * featureName,
-		LicenseLocation licenseLocation);
+EVENT_TYPE release_license(char* featureName, LicenseLocation licenseLocation);
 
 #ifdef __cplusplus
 }

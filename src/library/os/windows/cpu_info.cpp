46e2f06fa6f9a990e5b14194633ada42a18118e3
Gabriele Contini <1121667+gcontini@users.noreply.github.com>
2026-08-05 11:14:34 +0200

    issue #83 arm support added support for ARM architecture on Windows and Linux rename DmiInfo->BoardInfo improved on premise environment detection on Windows and Linux improved tests for execution_environment detection (Azure) Azure detection.

--- src/library/os/windows/cpu_info.cpp
+++ src/library/os/windows/cpu_info.cpp
@@ -5,15 +5,59 @@
  *      Author: devel
  */
 
-#include <intrin.h>
 #include <string>
 #include <unordered_set>
+#include <cstring>
 #include "../cpu_info.hpp"
 
-namespace license {
-namespace os {
-using namespace std;
+#ifdef _M_ARM64
+#include <windows.h>
+#include <winreg.h>
+#else
+#include <intrin.h>
+#endif
 
+	namespace license {
+	namespace os {
+	using namespace std;
+
+#ifdef _M_ARM64
+	static string get_cpu_vendor() {
+		// On ARM64, get CPU vendor from registry
+		HKEY hKey;
+		LONG lRes = RegOpenKeyExW(HKEY_LOCAL_MACHINE, L"HARDWARE\\DESCRIPTION\\System\\CentralProcessor\\0", 0,
+								  KEY_READ, &hKey);
+
+		string result = "Unknown";
+		if (lRes == ERROR_SUCCESS) {
+			wchar_t identifier[256] = {0};
+			DWORD dataSize = sizeof(identifier);
+
+			// Try to get the identifier which contains vendor info
+			lRes = RegQueryValueExW(hKey, L"Identifier", NULL, NULL, (LPBYTE)identifier, &dataSize);
+			if (lRes == ERROR_SUCCESS) {
+				// Convert wide string to regular string
+				int len = wcslen(identifier);
+				result.resize(len);
+				for (int i = 0; i < len; ++i) {
+					result[i] = static_cast<char>(identifier[i]);
+				}
+			} else {
+				// If identifier not available, try VendorIdentifier
+				lRes = RegQueryValueExW(hKey, L"VendorIdentifier", NULL, NULL, (LPBYTE)identifier, &dataSize);
+				if (lRes == ERROR_SUCCESS) {
+					int len = wcslen(identifier);
+					result.resize(len);
+					for (int i = 0; i < len; ++i) {
+						result[i] = static_cast<char>(identifier[i]);
+					}
+				}
+			}
+			RegCloseKey(hKey);
+		}
+		return result;
+	}
+#else
 static string get_cpu_vendor() {
 	// hypervisor flag false, try to get the vendor name, see if it's a virtual cpu
 	int cpui[4] = {0};
@@ -26,7 +70,37 @@ static string get_cpu_vendor() {
 	*reinterpret_cast<int*>(vendor + 8) = cpui[2];
 	return string(vendor, 12);
 }
+#endif
 
+#ifdef _M_ARM64
+	static string get_cpu_brand() {
+		// On ARM64, get CPU brand from registry
+		HKEY hKey;
+		LONG lRes = RegOpenKeyExW(HKEY_LOCAL_MACHINE, L"HARDWARE\\DESCRIPTION\\System\\CentralProcessor\\0", 0,
+								  KEY_READ, &hKey);
+
+		string result = "Unknown";
+		if (lRes == ERROR_SUCCESS) {
+			wchar_t processorName[256] = {0};
+			DWORD dataSize = sizeof(processorName);
+
+			// Fetch ProcessorNameString (e.g., "Snapdragon(TM) X Elite...")
+			lRes = RegQueryValueExW(hKey, L"ProcessorNameString", NULL, NULL, (LPBYTE)processorName, &dataSize);
+			if (lRes == ERROR_SUCCESS) {
+				// Convert wide string to regular string
+				int len = wcslen(processorName);
+				result.resize(len);
+				for (int i = 0; i < len; ++i) {
+					result[i] = static_cast<char>(processorName[i]);
+				}
+			} else {
+				result = "ARM Processor";
+			}
+			RegCloseKey(hKey);
+		}
+		return result;
+	}
+#else
 static string get_cpu_brand() {
 	int cpui[4] = {0};
 	__cpuid(cpui, 0x80000000);
@@ -48,35 +122,69 @@ static string get_cpu_brand() {
 
 	return result;
 }
+#endif
 
-CpuInfo::CpuInfo() : m_vendor(get_cpu_vendor()), m_brand(get_cpu_brand()) {}
+	CpuInfo::CpuInfo() : m_vendor(get_cpu_vendor()), m_brand(get_cpu_brand()) {}
 
-CpuInfo::~CpuInfo() {}
-/**
- * Detect Virtual machine using hypervisor bit.
- * @return true if the cpu hypervisor bit is set to 1
- */
-bool CpuInfo::is_virtual() const {
-	int cpui[4] = {0};
-	__cpuid(cpui, 0x1);
+	CpuInfo::~CpuInfo() {}
+	/**
+	 * Detect Virtual machine using hypervisor bit.
+	 * @return true if the cpu hypervisor bit is set to 1
+	 */
+	bool CpuInfo::is_virtual() const {
+		int cpui[4] = {0};
+		__cpuid(cpui, 0x1);
 
-	return ((cpui[2] >> 31) & 1);
-}
+		return ((cpui[2] >> 31) & 1);
+#endif
+	}
 
-/**
- * Check if virtualization information is available.
- * @return true for Windows and Linux Intel processors, false for ARM
- */
-bool CpuInfo::virt_info_available() const { return true; }
+	/**
+	 * Check if virtualization information is available.
+	 * @return true for Windows and Linux Intel processors, false for ARM
+	 */
+	bool CpuInfo::virt_info_available() const {
+#ifdef _M_ARM64
+		return false;
+#else
+	return true;
+#endif
+	}
 
-uint32_t CpuInfo::model() const {
+	uint32_t CpuInfo::model() const {
+#ifdef _M_ARM64
+		// On ARM64, we can get model information from registry
+		HKEY hKey;
+		LONG lRes = RegOpenKeyExW(HKEY_LOCAL_MACHINE, L"HARDWARE\\DESCRIPTION\\System\\CentralProcessor\\0", 0,
+								  KEY_READ, &hKey);
+
+		uint32_t result = 0;
+		if (lRes == ERROR_SUCCESS) {
+			DWORD modelNumber = 0;
+			DWORD dataSize = sizeof(modelNumber);
+
+			// Try to get the Architecture value
+			lRes = RegQueryValueExW(hKey, L"~MACHINEHALNAME", NULL, NULL, (LPBYTE)&modelNumber, &dataSize);
+			if (lRes != ERROR_SUCCESS) {
+				// If not available, try Architecture value
+				lRes = RegQueryValueExW(hKey, L"Architecture", NULL, NULL, (LPBYTE)&modelNumber, &dataSize);
+				if (lRes != ERROR_SUCCESS) {
+					modelNumber = 0;  // Default value if not found
+				}
+			}
+			result = static_cast<uint32_t>(modelNumber);
+			RegCloseKey(hKey);
+		}
+		return result;
+#else
 	int cpui[4] = {0};
 	__cpuid(cpui, 0x1);
 	// ax bits 0-3 stepping,4-7 model,8-11 family id,12-13 processor type
 	//        14-15 reserved, 16-19 extended model, 20-27 extended family, 27-31 reserved
 	// bx bits 0-7 brand index
 	return (cpui[0] & 0x3FFF) | (cpui[0] & 0x3FF8000) >> 2 | (cpui[1] & 0xff) << 24;
-}
+#endif
+	}
 
-}  // namespace os
+	}  // namespace os
 } /* namespace license */
